ddc1836838b6de0d84959e08851054127694b374
mluker <luker.matt@gmail.com>
2013-08-06 15:20:48 -0500

    added new playing card

---
+++ Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Matt Luker on 8/5/13.
+//  Copyright (c) 2013 Matt Luker. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
---
+++ Matchismo/Model/PlayingCardDeck.m
@@ -0,0 +1,27 @@
+//
+//  PlayingCardDeck.m
+//  Matchismo
+//
+//  Created by Matt Luker on 8/5/13.
+//  Copyright (c) 2013 Matt Luker. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
+
+@implementation PlayingCardDeck
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        for (NSString *suit in [PlayingCard validSuits]) {
+            for (NSUInteger rank = 1; rank <= [PlayingCard maxRank]; rank++) {
+                PlayingCard *card = [[PlayingCard alloc] init];
+                card.rank = rank;
+                card.suit = suit;
+                [self addCard:card atTop:YES];
+            }
+        } }
+    return self;
+}
+@end
